8b1a131b8347c50006e827534b138fcd0b205dd8
Hussein Fanjary <138877133+GisHussein@users.noreply.github.com>
2026-09-12 15:31:22 +0300

    Create __init__.py

---
+++ gisqa/checks/__init__.py
@@ -0,0 +1,6 @@
+from .crs import check_crs
+from .geometry import check_geometry
+from .schema import check_schema
+from .topology import check_topology
+
+__all__ = ["check_crs", "check_geometry", "check_schema", "check_topology"]
